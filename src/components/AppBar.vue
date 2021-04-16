28f4cf79748dc0444878ee148c95264a7b894419
daniel-keogh <37158241+daniel-keogh@users.noreply.github.com>
2021-04-16 11:21:31 +0100

    Add an AppBar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <b-navbar centered shadow>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <h1 class="subtitle is-5">Gestures Project</h1>
+        </b-navbar-item>
+      </template>
+
+      <template #start>
+        <b-navbar-item active href="#">Home</b-navbar-item>
+        <b-navbar-item href="#">Help</b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <b-button
+              class="is-light"
+              icon-left="github"
+              tag="a"
+              href="https://github.com/daniel-keogh/gestures-project"
+              target="_blank"
+            >
+              GitHub
+            </b-button>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <app-bar />
+    <main></main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AppBar from "@/components/AppBar";
 
 export default {
   name: "Home",
+
   components: {
-    HelloWorld,
+    AppBar,
   },
 };
 </script>
+
+<style lang="scss" scoped></style>
